9311a36bcfc731db3140472b9a1e31dbcd36cdfc
yan-valadares <yancardoso@outlook.com.br>
2023-06-19 17:17:52 -0300

    Criando a rota post aluno

--- routes.js
+++ routes.js
@@ -2,6 +2,10 @@ const express = require('express')
 const Professor = require('./database/models/ModeloProfessor')
 const Categoria = require('./database/models/ModeloCategoria')
 const Projeto= require('./database/models/ModeloProjeto')
+const Aluno = require('./database/models/ModeloAluno')
+const Responsavel= require('./database/models/ModeloResponsavel')
+const TelefoneResponsavel= require('./database/models/ModeloTelefoneResponsavel')
+const AlunoResponsavel= require('./database/models/ModeloAlunoResponsavel')
 const {randomUUID} = require('crypto')
 
 const routes = express.Router();
@@ -30,12 +34,58 @@ routes.get("/projectWinner.html", (req, res) => {
     res.sendFile(__dirname + "/public/html/projectWinner.html")
 })
 
-routes.post("/personSubscribe/post", (req, res) => {
-    
-    res.send(req.body)
+routes.post("/personSubscribe/post", async (req, res) => {
+
+    const capitalize = str => {
+        if (typeof str !== 'string') {
+            return '';
+        }
+        return str.charAt(0).toUpperCase() + str.substr(1);
+    }
+
+    const dataNascimento = new Date(req.body.data_nascimento)
+    let idAluno = randomUUID()
+    let idResponsavel = randomUUID()
+
+    const projetoData = await Projeto.findOne({
+        where: {
+            nome_projeto: capitalize(req.body.nome_projeto)
+        }
+    })
+
+    await Aluno.create({
+        id: idAluno,
+        nome_aluno: capitalize(req.body.nome_aluno),
+        sobrenome_aluno: capitalize(req.body.sobrenome_aluno),
+        prontuario: req.body.prontuario,
+        data_nascimento: dataNascimento,
+        id_projeto: projetoData.id
+    })
+
+    await Responsavel.create({
+        id: idResponsavel,
+        nome_responsavel: capitalize(req.body.nome_responsavel),
+        sobrenome_responsavel: capitalize(req.body.sobrenome_responsavel),
+        uf_responsavel: req.body.uf_responsavel.toUpperCase(),
+        cidade_responsavel: capitalize(req.body.cidade_responsavel),
+        logradouro_responsavel: capitalize(req.body.logradouro_responsavel)
+    })
+
+    await TelefoneResponsavel.create({
+        id: randomUUID(),
+        telefone: req.body.telefone,
+        id_responsavel: idResponsavel
+    })
+
+    await AlunoResponsavel.create({
+        id: randomUUID(),
+        id_aluno: idAluno,
+        id_responsavel: idResponsavel,
+        parentesco: capitalize(req.body.parentesco)
+    })
 
     
-    // res.sendFile(__dirname + "/public/html/subscribeConfirmation.html")
+    res.sendFile(__dirname + "/public/html/subscribeConfirmation.html")
 })
 
 routes.post("/projectSubscribe/post", async (req, res) => {
@@ -61,7 +111,7 @@ routes.post("/projectSubscribe/post", async (req, res) => {
     if(categoriaData != null && professorData != null){
         await Projeto.create({
             id: randomUUID(),
-            nome_projeto: req.body.nome_projeto,
+            nome_projeto: capitalize(req.body.nome_projeto),
             tema_projeto: req.body.tema_projeto,
             descricao: req.body.descricao,
             id_professor: professorData.id,
